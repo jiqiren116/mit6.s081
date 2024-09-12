29d04e03ee567505a6d164de2c92dd0377a608a4
Cao Mingyuan <1139401422@qq.com>
2024-09-12 18:15:13 +0800

    add 自己写的第一个程序，即hello world程序

---
+++ user/helloworld.c
@@ -0,0 +1,13 @@
+/*
+ * 这是我写的第一个程序，是跟着网上抄的，具体是https://blog.csdn.net/ConstineWhy/article/details/123313378
+*/
+
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main()
+{
+    printf("Hello World!\n");
+    exit(0); // 不能用return 0; 会报错
+}
